544b36af2725e664009e6d8f2080660b717db126
niklasrammelkamp <niklas.rammelkammp@gmail.com>
2023-01-21 13:15:54 +0100

    create Array of objects

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,14 @@
+import initialPictures from "@/store";
+
 export default function HomePage() {
-  return <h1>Hello from Next.js</h1>;
+  return (
+    <>
+      <h1>Foto App</h1>
+      <section>
+        {initialPictures.map((picture) => {
+          return <p key={picture.id}>{picture.image}</p>;
+        })}
+      </section>
+    </>
+  );
 }
