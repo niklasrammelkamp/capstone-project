28643a4a3c04ceb3e95884de082e02dd2a242415
niklasrammelkamp <niklas.rammelkammp@gmail.com>
2023-01-31 17:27:49 +0100

    adjust styling

--- components/SytledButton/index.js
+++ components/SytledButton/index.js
@@ -4,6 +4,7 @@ const { default: styled } = require("styled-components");
 
 const StyledButton = styled.button`
   border: none;
+  transition: 0.5s;
 
   ${({ variant, isActive }) => {
     if (variant === "navButton") {
@@ -13,14 +14,19 @@ const StyledButton = styled.button`
         flex-direction: column;
         align-items: center;
         gap: 0.5rem;
-        margin-top: 3px;
+        margin-top: ${isActive ? "-5px" : "0"};
 
         &::after {
+          position: absolute;
+          bottom: ${isActive ? "0.7rem" : "0"};
+          opacity: ${isActive ? "1" : "0"};
           content: "";
           display: inline-block;
           transition: 0.5s;
-          width: ${isActive ? "5px" : "0"};
-          height: ${isActive ? "5px" : "0"};
+          width: 5px;
+          height: 5px;
+          /* width: ;
+          height: ${isActive ? "5px" : "0"}; */
           border-radius: 50%;
           background-color: #86e211;
         }
@@ -28,11 +34,10 @@ const StyledButton = styled.button`
     }
     if (variant === "navButtonUpload") {
       return css`
-        transition: 0.5s;
         background-color: var(--black);
         height: 4rem;
         aspect-ratio: 1/1;
-        border-radius: 50%;
+        border-radius: 5rem;
         box-shadow: 0 0 2rem rgba(0, 0, 0, 0.15);
         transform: ${isActive ? "rotate(180deg)" : "none"};
         display: flex;
