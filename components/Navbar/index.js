51291383ef058dc44c0461552c550bdac54b8abc
niklasrammelkamp <niklas.rammelkammp@gmail.com>
2023-01-31 13:11:11 +0100

    create layout and navbar component

---
+++ components/Navbar/index.js
@@ -0,0 +1,12 @@
+import SVGIcon from "../SVGIcon";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div></div>
+      <button>
+        <SVGIcon variant="plus" width="20"></SVGIcon>
+      </button>
+    </nav>
+  );
+}
