27db9a9103537e6df67846066cd6f6b07304ffa1
niklasrammelkamp <niklas.rammelkammp@gmail.com>
2023-01-31 17:06:06 +0100

    fix small bug at main

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,10 +1,15 @@
+import styled from "styled-components";
 import Navbar from "../Navbar";
 
 export default function Layout({ children }) {
   return (
     <>
       <Navbar />
-      <main>{children}</main>
+      <StyledMain>{children}</StyledMain>
     </>
   );
 }
+
+const StyledMain = styled.main`
+  padding-bottom: 6rem;
+`;
