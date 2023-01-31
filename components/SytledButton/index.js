ca0054e7c2f839643b34330ed39495e0262ac8c9
niklasrammelkamp <niklas.rammelkammp@gmail.com>
2023-01-31 17:44:31 +0100

    adjust styling

--- components/SytledButton/index.js
+++ components/SytledButton/index.js
@@ -5,6 +5,7 @@ const { default: styled } = require("styled-components");
 const StyledButton = styled.button`
   border: none;
   transition: 0.5s;
+  padding: 0;
 
   ${({ variant, isActive }) => {
     if (variant === "navButton") {
@@ -25,8 +26,6 @@ const StyledButton = styled.button`
           transition: 0.5s;
           width: 5px;
           height: 5px;
-          /* width: ;
-          height: ${isActive ? "5px" : "0"}; */
           border-radius: 50%;
           background-color: #86e211;
         }
@@ -36,13 +35,19 @@ const StyledButton = styled.button`
       return css`
         background-color: var(--black);
         height: 4rem;
-        aspect-ratio: 1/1;
-        border-radius: 5rem;
+        aspect-ratio: 1 / 1;
+        border-radius: 999px;
         box-shadow: 0 0 2rem rgba(0, 0, 0, 0.15);
         transform: ${isActive ? "rotate(180deg)" : "none"};
         display: flex;
         align-items: center;
         justify-content: center;
+
+        /* padding-top: 0;
+        padding-left: 0;
+        padding-bottom: 0;
+        padding-right: 0; */
+        padding: 0;
       `;
     }
   }}
